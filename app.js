c0ed03adb39b78fe8660c94453dd3de8f4194bee
Pushkar Raj <imperial.raj.pushkar@gmail.com>
2017-06-17 08:06:16 +0000

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-var app=angular.module('myApp',[]);
+var app=angular.module('myApp',['ngRoute']);
 
-app.config(function(){
-    Stamplay.init("blogitpushkar")
-})
+app.config(function($routeProvider, $locationProvider){
+    Stamplay.init("blogitpushkar");
+    
+    $locationProvider.hashPrefix('');
+    
+    $routeProvider
+    .when('/',{
+            templateUrl: 'templates/home.html',
+            controller: "HomeCtrl"
+    })
+    .when('/login',{
+            templateUrl: 'templates/login.html',
+            controller: "LoginCtrl"
+    });
+});
+
+app.controller('HomeCtrl',function(){
+});
+
+app.controller('LoginCtrl',function(){
+});
 
 app.controller('myCtrl',function($scope){
-	$scope.message="Welcome to BlogIt!"
+// 	$scope.message="Welcome to BlogIt!"
 })
