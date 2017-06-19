170c55db166c52b10d727665f2f8ebf07dff8ce3
Pushkar Raj <imperial.raj.pushkar@gmail.com>
2017-06-19 17:26:12 +0000

    fourth commit

--- app.js
+++ app.js
@@ -26,7 +26,22 @@ app.controller('HomeCtrl',function(){
 app.controller('LoginCtrl',function(){
 })
 
-app.controller('SignUpCtrl',function() {
+app.controller('SignUpCtrl',function($scope) {
+    $scope.newUser = {};
+    $scope.signup = function(){
+        if($scope.newUser.firstName && $scope.newUser.lastName && $scope.newUser.email && $scope.newUser.password && $scope.newUser.confirmPassword){
+            console.log("All fields valid!");
+            if($scope.newUser.password == $scope.newUser.confirmPassword){
+                console.log("All good!");
+            }
+            else{
+                console.log("Passwords do not match!");
+            }
+        }    
+        else{
+            console.log("Some fields invalid!");
+        }    
+    };
 })
 
 app.controller('myCtrl',function($scope){
