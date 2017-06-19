099c6f657da8ac4b93dcc41ec65db38d4f5d3e45
Pushkar Raj <imperial.raj.pushkar@gmail.com>
2017-06-19 15:47:28 +0000

    third commit

--- app.js
+++ app.js
@@ -7,20 +7,27 @@ app.config(function($routeProvider, $locationProvider){
     
     $routeProvider
     .when('/',{
-            templateUrl: 'templates/home.html',
-            controller: "HomeCtrl"
+        templateUrl: 'templates/home.html',
+        controller: "HomeCtrl"
     })
     .when('/login',{
-            templateUrl: 'templates/login.html',
-            controller: "LoginCtrl"
-    });
-});
+        templateUrl: 'templates/login.html',
+        controller: "LoginCtrl"
+    })
+    .when('/signup',{
+        templateUrl: 'templates/signup.html',
+        controller: "SignUpCtrl"
+    })
+})
 
 app.controller('HomeCtrl',function(){
-});
+})
 
 app.controller('LoginCtrl',function(){
-});
+})
+
+app.controller('SignUpCtrl',function() {
+})
 
 app.controller('myCtrl',function($scope){
 // 	$scope.message="Welcome to BlogIt!"
