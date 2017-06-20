497a5d3a25e347aa6af3bba4ec21e3970407484f
Pushkar Raj <imperial.raj.pushkar@gmail.com>
2017-06-20 18:39:24 +0000

    fifth commit

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var app=angular.module('myApp',['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider){
     Stamplay.init("blogitpushkar");
-    
+    localStorage.removeItem('https://blogit-pushkarraj.c9users.io-jwt');
     $locationProvider.hashPrefix('');
-    
     $routeProvider
     .when('/',{
         templateUrl: 'templates/home.html',
@@ -18,13 +17,44 @@ app.config(function($routeProvider, $locationProvider){
         templateUrl: 'templates/signup.html',
         controller: "SignUpCtrl"
     })
-})
+    .when('/viewBlogs',{
+        templateUrl: 'templates/viewBlogs.html',
+        controller: "ViewBlogsCtrl"
+    })
+});
 
 app.controller('HomeCtrl',function(){
-})
+});
 
-app.controller('LoginCtrl',function(){
-})
+app.controller('LoginCtrl',function($scope){
+    $scope.login = function(){
+        Stamplay.User.currentUser()
+        .then(function(res){
+            console.log(res);
+            if(res.user){
+                $timeout(function(){
+                    $location.path("/viewBlogs");
+                });    
+            }
+            else{
+                Stamplay.User.login($scope.user)
+                .then(function(res){
+                    console.log("Logged in "+res);
+                    $timeout(function() {
+                        $location.path("/viewBlogs");
+                    });
+                },
+                function(err){
+                    console
+                    .log(err);
+                })
+            }
+        },
+        function(error){
+            console.log(error);
+        });
+    };
+});
 
 app.controller('SignUpCtrl',function($scope) {
     $scope.newUser = {};
@@ -33,6 +63,13 @@ app.controller('SignUpCtrl',function($scope) {
             console.log("All fields valid!");
             if($scope.newUser.password == $scope.newUser.confirmPassword){
                 console.log("All good!");
+                Stamplay.User.signup($scope.newUser)
+                .then(function(response){
+                    console.log(response);
+                }, 
+                function(error){
+                    console.log(error);
+                });
             }
             else{
                 console.log("Passwords do not match!");
@@ -42,8 +79,12 @@ app.controller('SignUpCtrl',function($scope) {
             console.log("Some fields invalid!");
         }    
     };
-})
+});
+
+app.controller('ViewBlogsCtrl',function(){
+    
+});
 
 app.controller('myCtrl',function($scope){
 // 	$scope.message="Welcome to BlogIt!"
-})
+});
