fa9c7fc22c493fc43709ef9f45c912da042489c8
Pushkar Raj <imperial.raj.pushkar@gmail.com>
2017-07-12 13:05:45 +0000

    eighth commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var app=angular.module('myApp',['ui.router','ngToast']);
+var app=angular.module('myApp',['ui.router','ngToast','textAngular']);
 
-app.run(function($rootScope){
+app.run(function($rootScope, $state, AuthService, $transitions){
+    /*
     Stamplay.User.currentUser()
     .then(function(res){
        if(res.user){
@@ -15,11 +16,61 @@ app.run(function($rootScope){
     function(err){
         console.log("An error occurred while getting current user!");
     });
+    */
+    /*
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+        if(toState.authenticate == true)
+        {
+            AuthService.isAuthenticated()
+            .then(function(res){
+                if(res == false)
+                {
+                    $state.go('login');
+                }
+            });
+        }
+    })
+    */
+    $transitions.onStart({}, function(transition){
+        if(transition.$to().self.authenticate == true){
+            AuthService.isAuthenticated()
+            .then(function(res){
+                console.log(res);
+                if(res == false)
+                {
+                    $state.go('login');
+                }
+            });
+        }
+    })
+});
+
+app.factory('AuthService', function($q, $rootScope){
+   return{
+       isAuthenticated : function(){
+           var defer = $q.defer();
+           Stamplay.User.currentUser(function(err, res){
+               if(err){
+                   defer.resolve(false);
+                   $rootScope.loggedIn = false;
+               }
+               if(res.user){
+                   defer.resolve(true);
+                   $rootScope.loggedIn = true;
+               }
+               else{
+                   deferr.resolve(false);
+                   $rootScope.loggedIn = false;
+               }
+           });
+           return defer.promise;
+       }
+   } 
 });
 
 app.config(function($stateProvider, $locationProvider, $urlRouterProvider){
-    Stamplay.init("blogitpushkar");
-    localStorage.removeItem('https://blogit-pushkarraj.c9users.io-jwt');
+    Stamplay.init("blogitpushkar2");
+    //localStorage.removeItem('https://blogit-pushkarraj.c9users.io-jwt');
     $locationProvider.hashPrefix('');
     $stateProvider
         .state('home', {
@@ -40,12 +91,40 @@ app.config(function($stateProvider, $locationProvider, $urlRouterProvider){
         .state('MyBlogs', {
             url: '/myBlogs',
             templateUrl: 'templates/myBlogs.html',
-            controller: "MyBlogsCtrl"
+            controller: "MyBlogsCtrl",
+            authenticate: true
+        })
+        .state('Create', {
+            url: '/create',
+            templateUrl: 'templates/create.html',
+            controller: "CreateCtrl",
+            authenticate: true
+        })
+        .state('Edit', {
+            url: '/edit/:id',
+            templateUrl: 'templates/edit.html',
+            controller: "EditCtrl",
+            authenticate: true
+        })
+        .state('view', {
+            url: '/view/:id',
+            templateUrl: 'templates/view.html',
+            controller: "ViewCtrl"
         });
     $urlRouterProvider.otherwise("/");
 });
 
-app.controller('HomeCtrl', function(){
+app.controller('HomeCtrl', function($scope, $http){
+    Stamplay.Object("blogs").get({sort : "-dt_create"})
+    .then(function(res){
+        console.log(res);
+        $scope.latestBlogs = res.data;
+        $scope.$apply();
+        console.log($scope.latestBlogs);
+    },
+    function(err){
+        console.log(err);
+    });
 });
 
 app.controller('LoginCtrl', function($scope, $state, $timeout, $rootScope, ngToast){
@@ -63,15 +142,14 @@ app.controller('LoginCtrl', function($scope, $state, $timeout, $rootScope, ngToa
             else{
                 Stamplay.User.login($scope.user)
                 .then(function(res){
-                    console.log("Logged in "+res);
+                    console.log(res);
+                    
+                    $rootScope.loggedIn = true;
+                    $rootScope.displayName = res.firstName+" "+res.lastName;
                     $timeout(function() {
                         ngToast.create("Logged in successfully!");
-                    });
-                    $rootScope.loggedIn = true;
-                    $rootScope.displayName = res.user.firstName+" "+res.user.lastName;
-                    $timeout(function(){
                         $state.go('MyBlogs');
-                    });
+                    });                
                 },
                 function(err){
                     console.log(err);
@@ -94,6 +172,7 @@ app.controller('LoginCtrl', function($scope, $state, $timeout, $rootScope, ngToa
 app.controller('SignUpCtrl', function($scope, $timeout, ngToast) {
     $scope.newUser = {};
     $scope.signup = function(){
+        $scope.newUser.displayName = $scope.newUser.firstName + " " + $scope.newUser.lastName;
         if($scope.newUser.firstName && $scope.newUser.lastName && $scope.newUser.email && $scope.newUser.password && $scope.newUser.confirmPassword){
             console.log("All fields valid!");
             if($scope.newUser.password == $scope.newUser.confirmPassword){
@@ -128,9 +207,199 @@ app.controller('SignUpCtrl', function($scope, $timeout, ngToast) {
     };
 });
 
-app.controller('MyBlogsCtrl', function($scope) {
-    
+app.controller('CreateCtrl', function(taOptions, $state, $scope, $timeout, ngToast){
+    $scope.newPost={};
+    taOptions.toolbar = [
+      ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+      ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
+      ['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent'],
+      ['html', 'insertImage','insertLink', 'insertVideo', 'wordcount', 'charcount']
+  ];
+  $scope.Create = function(){
+      Stamplay.User.currentUser()
+      .then(function(res){
+        if(res.user){
+           Stamplay.Object("blogs").save($scope.newPost)
+           .then(function(res){
+               $timeout(function() {
+                   ngToast.create("Post created successfully!");
+               })
+               $state.go('MyBlogs');
+           }, function(err){
+               $timeout(function() {
+                   ngToast.create("An error has occured while creating the post! Please try later.")
+               });
+               console.log(err);
+           })
+        }
+        else{
+            $state.go('login');
+        }
+      }, function(err){    
+            $timeout(function(){
+                ngToast.create("An error has occurred! Please try later.");
+            });
+            console.log(err);
+        })
+    }
 });
+
+app.controller('MyBlogsCtrl', function($scope, $state) {
+    Stamplay.User.currentUser(function(err, res){
+        if(res.user){
+            Stamplay.Object("blogs").get({owner: res.user._id, sort : "-dt_create"})
+            .then(function(response){
+                console.log(response);
+                $scope.userBlogs = response.data;
+                $scope.$apply();
+                console.log($scope.userBlogs);
+            }, 
+            function(err){
+                console.log(err);
+            });
+        }
+        else{
+            $state.go('login');
+        }
+    },
+    function(err){
+        console.log(err);
+    });    
+});
+
+app.controller('EditCtrl', function(taOptions, $scope, $state, $stateParams, $timeout, ngToast){
+    $scope.Post = {};
+    taOptions.toolbar = [
+      ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+      ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
+      ['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent'],
+      ['html', 'insertImage','insertLink', 'wordcount', 'charcount']
+    ];
+    Stamplay.Object("blogs").get({_id: $stateParams.id})
+    .then(function(res){
+       console.log(res);
+       $scope.Post = res.data[0];
+       $scope.$apply();
+       console.log($scope.Post);
+    },
+    function(err) {
+        console.log(err);
+    });
+    $scope.update = function(){
+        Stamplay.User.currentUser().then(function(res){
+            if(res.user){
+                if(res.user._id == $scope.Post.owner){
+                    Stamplay.Object("blogs").update($stateParams.id, $scope.Post)
+                    .then(function(response){
+                        console.log(response);
+                        $state.go("MyBlogs");
+                    },
+                    function(error){
+                        console.log(error);
+                    });
+                }
+                else
+                    $state.go("login");
+            }
+            else
+                $state.go("login");
+        },
+        function(err){
+            console.log(err);
+        });
+    }
+});
+
+app.filter('htmlToPlainText', function(){
+    return function(text){
+        return text ? String(text).replace(/<[^>]+>/gm, '') : '';
+    }
+})
+
+app.controller('ViewCtrl', function($scope, $stateParams, $timeout, $state, ngToast){
+    $scope.upVoteCount = 0;
+    $scope.downVoteCount = 0;
+    Stamplay.Object("blogs").get({_id: $stateParams.id})
+    .then(function(response){
+        $scope.blog = response.data[0];
+        $scope.upVoteCount = $scope.blog.actions.votes.users_upvote.length;
+        $scope.downVoteCount = $scope.blog.actions.votes.users_downvote.length;
+        $scope.$apply();
+    },
+    function(error){
+        console.log(error);
+    });
+    $scope.postComment = function(){
+        Stamplay.Object("blogs").comment($stateParams.id, $scope.comment)
+        .then(function(res){
+            console.log(res);
+            $scope.blog = res;
+            $scope.comment = "";
+            $scope.$apply();
+        },
+        function(err){
+            console.log(err);
+            if(err.code == 403){
+                console.log("Please login first!");
+                $timeout(function(){
+                    ngToast.create('<a href="#/login" class="">Please login before posting comments!</a>');
+                });
+            }
+        })
+    }
+    $scope.upVote = function(){
+        Stamplay.Object("blogs").upVote($stateParams.id)
+        .then(function(res){
+            console.log(res);
+            $scope.blog = res;
+            $scope.comment = "";
+            $scope.upVoteCount = $scope.blog.actions.votes.users_upvote.length;
+            $scope.downVoteCount = $scope.blog.actions.votes.users_downvote.length;
+            $scope.$apply();
+        },
+        function(err){
+            console.log(err);
+            if(err.code == 403){
+                console.log("Please login first!");
+                $timeout(function(){
+                    ngToast.create('<a href="#/login" class="">Please login before posting comments!</a>');
+                });
+            }
+            if(err.code == 406){
+                console.log("Already voted!");
+                $timeout(function(){
+                    ngToast.create('You have already voted on this post!');
+                });
+            }
+        })
+    }
+    $scope.downVote = function(){
+        Stamplay.Object("blogs").downVote($stateParams.id)
+        .then(function(res){
+            console.log(res);
+            $scope.blog = res;
+            $scope.comment = "";
+            $scope.upVoteCount = $scope.blog.actions.votes.users_upvote.length;
+            $scope.downVoteCount = $scope.blog.actions.votes.users_downvote.length;
+            $scope.$apply();
+        },
+        function(err){
+            console.log(err);
+            if(err.code == 403){
+                console.log("Please login first!");
+                $timeout(function(){
+                    ngToast.create('<a href="#/login" class="">Please login before posting comments!</a>');
+                });
+            }
+            if(err.code == 406){
+                console.log("Already voted!");
+                $timeout(function(){
+                    ngToast.create('You have already voted on this post!');
+                });
+            }
+        })
+    }
+})
 
 app.controller('MainCtrl', function($scope, $rootScope, $timeout, ngToast){
     $scope.logout = function(){
